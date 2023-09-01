c382886530d2ed13ee6889a66ca8554d3d43961c
Jashwanth Kadaru <jashwanthkadaru2021@gmail.com>
2023-09-02 04:30:06 +0530

    In server folder, updated model users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {insertUser, verifyUser} from '../utilities/User.js' 
-
+import {createCart} from '../utilities/Cart.js'
 const router = express.Router();
 
 // To verify Login
@@ -8,16 +8,21 @@ router.post("/login",async (req, res) => {
     
     const username = req.body.username;
     const password = req.body.password;
-    let user = null;
+
     try{
-         user = await verifyUser(username, password);
-         console.log(user);
+         const result = await verifyUser(username, password);
+         if(result.success) {
+            console.log("verification success.");
+            console.log({success: result.success, username: result.verifiedUser.userName, fullname: result.verifiedUser.userFullName, email: result.verifiedUser.userEmail});
+            res.json({success: result.success, username: result.verifiedUser.userName, fullname: result.verifiedUser.userFullName, email: result.verifiedUser.userEmail});
+         } else {
+            console.log("verification Failed.");
+            res.json({success: result.success, username: null, fullname: null, email: null});
+        }
     }catch(err){
         console.error(err);
         res.status(500).json();
     }
-    
-    res.json(user);
 })
 
 
@@ -26,13 +31,21 @@ router.post("/register", async(req, res) => {
     const params = req.body;
     
     try{
-        const newUser = await insertUser(params.username, params.password, params.email, params.fullname, params.picture);
+        const result = await insertUser(params.username, params.password, params.email, params.fullname, params.picture);
 
-        if(newUser) {
-            console.log("\n\nnew User:\n",newUser);
-            res.json({success: true, userId: newUser.userID});
+        if(result.success) {
+            console.log("\n\nnew User:\n",result.new_user);
+            const result2 = await createCart(params.username);
+            if(result2.success) {
+                console.log("cart created:",result2);
+                res.json({success: true, userId: newUser.userID});
+            } else {
+                console.log("cart created:",result2);
+                res.json({success: false, errorMessage: "Failed to create cart."});
+            }
         } else {
-            res.json({success: false, userId: null});
+            console.log("cart created:",result);
+            res.json({success: false, errorMessage: "Failed to create user."});
         }
     } catch(error) {
         res.status(500).json();
