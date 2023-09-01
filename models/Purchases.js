93be690ea95bc19f9efafcc18392f53fee5350d5
Jashwanth Kadaru <jashwanthkadaru2021@gmail.com>
2023-09-02 04:27:48 +0530

    In server folder, updated model Purchases.js

--- models/Purchases.js
+++ models/Purchases.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 const purchasesSchema = new mongoose.Schema({
   purchaseID: {type: String, unique: true},
-  purchaseName: String,
-  purchaseTotal: Number,
-  purchaseItems: String,
+  purchaseName: {type: String, required: true},
+  purchaseTotal: {type: Number, required: true},
+  purchaseItems: {type: mongoose.Schema.Types.Mixed, required: true},
   purchaseDateTime: {type: Date, default: Date.now()}
 });
 
