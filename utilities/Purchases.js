ae2d4ee0a68c0513561779cba0695a59a1aa6462
Jashwanth Kadaru <jashwanthkadaru2021@gmail.com>
2023-09-02 04:31:03 +0530

    In server folder, updated utilities purchases.js

--- utilities/Purchases.js
+++ utilities/Purchases.js
@@ -3,51 +3,6 @@ import Purchases from '../models/Purchases.js'
 import {retrieveProductById} from '../utilities/Product.js'
 import { v4 as uuidv4} from 'uuid';
 
-export async function insertPurchase( username, itemsList ) {
-    try{
-        let total = 0;
-        let itemsList1 = [];
-           
-        try{
-            for (const item of itemsList) {
-                const product = await retrieveProductById(item.productId);
-                const price = product.productPrice;
-
-                total += item.productQty * price;
-                itemsList1.push({
-                    productName: product.productName,
-                    productQty: item.productQty,
-                    totalCost: item.productQty * price,
-                });
-            }
-        } catch(error) {
-            throw error;
-        }
-
-        const purchaseId = uuidv4();
-        let itemsString = JSON.stringify(itemsList1);
-        const newPurchase = new Purchases({
-            purchaseID: purchaseId,
-            purchaseName: username,
-            purchaseTotal: total,
-            purchaseItems: itemsString
-        });
-
-        const purchase = await newPurchase.save();
-
-        if(purchase){
-            console.log('Purchase created:', purchase);
-            return purchase;
-        } else {
-            console.log('Failed to create purchase:');
-            return null;
-        }
-    } catch(error) {
-        console.error('Error creating purchase:', err);
-        throw error;
-    };
-} 
-
 export async function retrievePurchasesList(username) {
     try {
         const purchasesList = await Purchases.find({purchaseName: username})
@@ -66,4 +21,51 @@ export async function retrievePurchasesList(username) {
     }
 }
 
-export default {insertPurchase, retrievePurchasesList};
+export default {retrievePurchasesList};
+
+
+// Omitted code:
+// export async function insertPurchase( username, itemsList ) {
+//     try{
+//         let total = 0;
+//         let itemsList1 = [];
+           
+//         try{
+//             for (const item of itemsList) {
+//                 const product = await retrieveProductById(item.productId);
+//                 const price = product.productPrice;
+
+//                 total += item.productQty * price;
+//                 itemsList1.push({
+//                     productName: product.productName,
+//                     productQty: item.productQty,
+//                     totalCost: item.productQty * price,
+//                 });
+//             }
+//         } catch(error) {
+//             throw error;
+//         }
+
+//         const purchaseId = uuidv4();
+//         let itemsString = JSON.stringify(itemsList1);
+//         const newPurchase = new Purchases({
+//             purchaseID: purchaseId,
+//             purchaseName: username,
+//             purchaseTotal: total,
+//             purchaseItems: itemsString
+//         });
+
+//         const purchase = await newPurchase.save();
+
+//         if(purchase){
+//             console.log('Purchase created:', purchase);
+//             return purchase;
+//         } else {
+//             console.log('Failed to create purchase:');
+//             return null;
+//         }
+//     } catch(error) {
+//         console.error('Error creating purchase:', err);
+//         throw error;
+//     };
+// } 
